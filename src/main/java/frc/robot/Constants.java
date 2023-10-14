116aef22244786e825dbf6b2c0ee15309bea60c7
SamJinLi <lijinzesamuel@gmail.com>
2023-10-14 16:03:15 -0700

    Updated drivetrain variables to Constant.java
    Straight from Sean's code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,11 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.util.Units;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -26,9 +31,60 @@ public static final int MOTOR_RIGHT_FOLLOWER_ID = 2;
 // public static final int MOTOR_RIGHT_PRIMARY_ID = 9;
 // public static final int MOTOR_RIGHT_FOLLOWER_ID = 0;
 
+public static final class DriveConstants {
+
+  public static final double kTrackWidth = Units.inchesToMeters(21);
+  // Distance between right and left wheels
+  public static final double kWheelBase = Units.inchesToMeters(25);
+  // Distance between front and back wheels
+  public static final SwerveDriveKinematics kDriveKinematics = new SwerveDriveKinematics(
+          new Translation2d(kWheelBase / 2, -kTrackWidth / 2),
+          new Translation2d(kWheelBase / 2, kTrackWidth / 2),
+          new Translation2d(-kWheelBase / 2, -kTrackWidth / 2),
+          new Translation2d(-kWheelBase / 2, kTrackWidth / 2));
+
+  public static final int kFrontLeftDriveMotorPort = 7;
+  public static final int kBackLeftDriveMotorPort = 2;
+  public static final int kFrontRightDriveMotorPort = 0;
+  public static final int kBackRightDriveMotorPort = 5;
+
+  public static final int kFrontLeftTurningMotorPort = 3;
+  public static final int kBackLeftTurningMotorPort = 4;
+  public static final int kFrontRightTurningMotorPort = 1;
+  public static final int kBackRightTurningMotorPort = 6;
+
+  public static final boolean kFrontLeftTurningEncoderReversed = false;
+  public static final boolean kBackLeftTurningEncoderReversed = false;
+  public static final boolean kFrontRightTurningEncoderReversed = false;
+  public static final boolean kBackRightTurningEncoderReversed = false;
+
+  public static final boolean kFrontLeftDriveEncoderReversed = false;
+  public static final boolean kBackLeftDriveEncoderReversed = false;
+  public static final boolean kFrontRightDriveEncoderReversed = false;
+  public static final boolean kBackRightDriveEncoderReversed = false;
+
+  public static final int kFrontLeftDriveAbsoluteEncoderPort = 1;
+  public static final int kBackLeftDriveAbsoluteEncoderPort = 0;
+  public static final int kFrontRightDriveAbsoluteEncoderPort = 3;
+  public static final int kBackRightDriveAbsoluteEncoderPort = 2;
+
+  public static double kFrontLeftDriveAbsoluteEncoderOffsetDeg = 0;
+  public static double kBackLeftDriveAbsoluteEncoderOffsetDeg = 0;
+  public static double kFrontRightDriveAbsoluteEncoderOffsetDeg = 0;
+  public static double kBackRightDriveAbsoluteEncoderOffsetDeg = 0;
+
+  public static final double kPhysicalMaxSpeedMetersPerSecond = 10;
+  public static final double kPhysicalMaxAngularSpeedRadiansPerSecond = 6 * Math.PI;
+
+  public static final double kTeleDriveMaxSpeedMetersPerSecond = kPhysicalMaxSpeedMetersPerSecond;
+  public static final double kTeleDriveMaxAngularSpeedRadiansPerSecond = kPhysicalMaxAngularSpeedRadiansPerSecond / 2;
+  public static final double kTeleDriveMaxAccelerationUnitsPerSecond = 3;
+  public static final double kTeleDriveMaxAngularAccelerationUnitsPerSecond = 3;
+}
+
 public static final class ArmConstants{
     // TODO: change these stuff as needed
-    public static final int k_ARM_DRIVE_LEADER_ID = 16;
+    public static final int arm_ID = 1;
     //Uncomment k_MOTORS_REVERSED if true
     public static final boolean k_MOTORS_REVERSED = true;
     public class ControlType{
